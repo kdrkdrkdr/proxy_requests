4faf73fb9685a188cd72113cdf033d758f27356a
kdrkdrkdr <kdrhacker1234@gmail.com>
2020-01-31 16:49:21 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # proxy_requests
+
+# 밤에 리드미 작성할거임
---
+++ proxy_requests.py
@@ -0,0 +1,137 @@
+#-*- coding:utf-8 -*-
+
+import sys
+import os
+import requests
+import subprocess
+import psutil
+import urllib
+
+
+SNI_DNS_SERVER_ADDRESS = ""
+
+class TorRequests(object):
+    """
+This class makes web requests with Tor Proxy.
+    """
+    
+    def __init__(self):
+        self._tor_is_alive()
+        
+        self.tor_session = requests.Session()
+        self.tor_session.proxies.update(
+            {
+                "http"  : "socks5://127.0.0.1:9050",
+                "https" : "socks5://127.0.0.1:9050",
+            }
+        )
+
+
+    def _tor_is_alive(self):
+        self.proc_list = [proc.name() for proc in psutil.process_iter()]
+        
+        if os.name == 'nt':
+            if 'tor.exe' in self.proc_list:
+                return True
+            else:
+                subprocess.Popen(['./TorService/tor.exe'])
+
+        else:
+            sys.exit("This module does not support Linux yet.")
+
+
+    def get(self, *args, **kwargs):
+        return self.tor_session.get(*args, **kwargs)
+
+
+    def post(self, *args, **kwargs):
+        return self.tor_session.post(*args, **kwargs)
+
+
+    def put(self, *args, **kwargs):
+        return self.tor_session.put(*args, **kwargs)
+
+
+    def patch(self, *args, **kwargs):
+        return self.tor_session.patch(*args, **kwargs)
+
+
+    def close(self):
+        self.tor_session.close()
+
+
+    def delete(self, *args, **kwargs):
+        return self.tor_session.delete(*args, **kwargs)
+
+
+
+class SniRequests(object):
+    """
+This class makes web reqeusts by bypass the SNI block.
+    """
+
+    def __init__(self):
+        self.sni_session = requests.Session()
+        self._Enable_SNI_Adapter = _Enable_SNI_Adapter()
+        self.sni_session.mount('https://', self._Enable_SNI_Adapter)
+
+
+    def get(self, *args, **kwargs):
+        return self.sni_session.get(*args, **kwargs)
+
+
+    def post(self, *args, **kwargs):
+        return self.sni_session.post(*args, **kwargs)
+
+
+    def put(self, *args, **kwargs):
+        return self.sni_session.put(*args, **kwargs)
+
+
+    def patch(self, *args, **kwargs):
+        return self.sni_session.patch(*args, **kwargs)
+
+
+    def close(self):
+        self.sni_session.close()
+
+
+    def delete(self, *args, **kwargs):
+        return self.sni_session.delete(*args, **kwargs)
+
+
+
+
+class _Enable_SNI_Adapter(requests.adapters.HTTPAdapter):
+
+    def send(self, request, **kwargs):
+        try:
+            connection_pool_kwargs = self.poolmanager.connection_pool_kw
+            result = urllib.parse.urlparse(request.url)
+            DNSADDR = SNI_DNS_SERVER_ADDRESS
+            
+            if result.scheme == 'https' and DNSADDR:
+                request.url = request.url.replace(
+                    'https://' + result.hostname,
+                    'https://' + DNSADDR,
+                )
+
+                connection_pool_kwargs['server_hostname'] = result.hostname 
+                connection_pool_kwargs['assert_hostname'] = result.hostname
+                request.headers['Host'] = result.hostname
+
+            else:
+                connection_pool_kwargs.pop('server_hostname', None)
+                connection_pool_kwargs.pop('assert_hostname', None)
+
+            return super(_Enable_SNI_Adapter, self).send(request, **kwargs)
+
+
+        except ( requests.exceptions.SSLError, requests.exceptions.ConnectionError ):
+            sys.exit(
+                "\nProxyRequests ERROR : [SSLError and ConnectionError]"
+                "\n+-------------------------------------------------------+"
+                "\n|1. Did you set the DNS address properly?               |"
+                "\n|2. Did you enter the address of the website correctly? |"
+                "\n+-------------------------------------------------------+\n"
+            )
